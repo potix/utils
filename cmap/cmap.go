25a51c06f3b23be260b406565be22cd1b07073cc
poti <poti.dog@gmail.com>
2021-04-25 06:36:46 +0900

    add test and fix bug

--- cmap/cmap.go
+++ cmap/cmap.go
@@ -1,7 +1,6 @@
 package cmap
 
 import (
-	"reflect"
 	"sync"
 )
 
@@ -19,12 +18,6 @@ func (cm *CMap) Len() (length int) {
 	cm.l.RLock()
 	defer cm.l.RUnlock()
 	return len(cm.m)
-}
-
-func (cm *CMap) IsEmpty() (empty bool) {
-	cm.l.RLock()
-	defer cm.l.RUnlock()
-	return len(cm.m) == 0
 }
 
 func (cm *CMap) Get(key interface{}) (value interface{}, ok bool) {
@@ -50,7 +43,7 @@ func (cm *CMap) SetIfAbsent(key interface{}, value interface{}) (ok bool) {
 	return !ok
 }
 
-func (cm *CMap) Replace(key interface{}, value interface{}) (ok bool) {
+func (cm *CMap) SetIfExist(key interface{}, value interface{}) (ok bool) {
 	cm.l.Lock()
 	defer cm.l.Unlock()
 	_, ok = cm.m[key]
@@ -60,21 +53,11 @@ func (cm *CMap) Replace(key interface{}, value interface{}) (ok bool) {
 	return ok
 }
 
-func (cm *CMap) GetDefault(key interface{}, defValue interface{}) (value interface{}) {
-	cm.l.RLock()
-	defer cm.l.RUnlock()
-	value, ok := cm.m[key]
-	if !ok {
-		return defValue
-	}
-	return value
-}
-
-func (cm *CMap) GetAndSetDefault(key interface{}, defValue interface{}) (value interface{}) {
+func (cm *CMap) GetWithDefault(key interface{}, defValue interface{}) (value interface{}) {
 	cm.l.Lock()
 	defer cm.l.Unlock()
 	value, ok := cm.m[key]
-	if ok {
+	if !ok {
 		cm.m[key] = defValue
 		return defValue
 	}
@@ -91,35 +74,29 @@ func (cm *CMap) Delete(key interface{}) (ok bool) {
 	return ok
 }
 
-func (cm *CMap) IsExistsKey(key interface{}) (ok bool) {
-	cm.l.RLock()
-	defer cm.l.RUnlock()
-	_, ok = cm.m[key]
-	return ok
-}
-
-func (cm *CMap) IsExistsValue(value interface{}) (ok bool) {
-	cm.l.RLock()
-	defer cm.l.RUnlock()
-	for _, iValue := range cm.m {
-		if reflect.DeepEqual(iValue, value) {
-			return true
-		}
-	}
-	return false
-}
-
 func (cm *CMap) Clear() {
 	cm.l.Lock()
 	defer cm.l.Unlock()
 	cm.m = make(map[interface{}]interface{})
 }
 
-func (cm *CMap) Merge(src *CMap) {
-	src.rLock()
+func (cm *CMap) OverwriteMerge(src *CMap) {
+	src.l.RLock()
 	cm.l.Lock()
 	defer func() {
-		src.rUnlock()
+		src.l.RUnlock()
+		cm.l.Unlock()
+	}()
+	for iKey, iValue := range src.m {
+		cm.m[iKey] = iValue
+	}
+}
+
+func (cm *CMap) KeepMerge(src *CMap) {
+	src.l.RLock()
+	cm.l.Lock()
+	defer func() {
+		src.l.RUnlock()
 		cm.l.Unlock()
 	}()
 	for iKey, iValue := range src.m {
@@ -129,24 +106,12 @@ func (cm *CMap) Merge(src *CMap) {
 	}
 }
 
-func (cm *CMap) Update(src *CMap) {
-	src.rLock()
-	cm.l.Lock()
-	defer func() {
-		src.rUnlock()
-		cm.l.Unlock()
-	}()
-	for iKey, iValue := range src.m {
-		cm.m[iKey] = iValue
-	}
-}
-
 func (cm *CMap) Copy() (newCMap *CMap) {
 	newCMap = NewCMap()
 	cm.l.RLock()
 	defer cm.l.RUnlock()
 	for iKey, iValue := range cm.m {
-		newCMap.unsafeSet(iKey, iValue)
+		newCMap.Set(iKey, iValue)
 	}
 	return newCMap
 }
@@ -154,8 +119,7 @@ func (cm *CMap) Copy() (newCMap *CMap) {
 func (cm *CMap) Keys() (keys []interface{}) {
 	cm.l.RLock()
 	defer cm.l.RUnlock()
-	length := len(cm.m)
-	keys = make([]interface{}, 0, length)
+	keys = make([]interface{}, 0, len(cm.m))
 	for iKey, _ := range cm.m {
 		keys = append(keys, iKey)
 	}
@@ -165,8 +129,7 @@ func (cm *CMap) Keys() (keys []interface{}) {
 func (cm *CMap) Values() (values []interface{}) {
 	cm.l.RLock()
 	defer cm.l.RUnlock()
-	length := len(cm.m)
-	values = make([]interface{}, 0, length)
+	values = make([]interface{}, 0, len(cm.m))
 	for _, iValue := range cm.m {
 		values = append(values, iValue)
 	}
@@ -176,8 +139,7 @@ func (cm *CMap) Values() (values []interface{}) {
 func (cm *CMap) Items() (items []*KeyValue) {
 	cm.l.RLock()
 	defer cm.l.RUnlock()
-	length := len(cm.m)
-	items = make([]*KeyValue, 0, length)
+	items = make([]*KeyValue, 0, len(cm.m))
 	for iKey, iValue := range cm.m {
 		items = append(items, &KeyValue{
 			Key:   iKey,
@@ -198,43 +160,6 @@ func (cm *CMap) Pop(k interface{}) (value interface{}, ok bool) {
 	return value, ok
 }
 
-func (cm *CMap) PopItem() (key interface{}, value interface{}, ok bool) {
-	cm.l.Lock()
-	defer cm.l.Unlock()
-	for iKey, iValue := range cm.m {
-		key = iKey
-		value = iValue
-		ok = true
-		break
-	}
-	if ok {
-		delete(cm.m, key)
-	}
-	return key, value, ok
-}
-
-func (cm *CMap) ForeachKey(cbfunc func(key interface{}) (loopBreak bool)) {
-	cm.l.Lock()
-	defer cm.l.Unlock()
-	for iKey, _ := range cm.m {
-		loopBreak := cbfunc(iKey)
-		if loopBreak {
-			break
-		}
-	}
-}
-
-func (cm *CMap) ForeachValue(cbfunc func(value interface{}) (loopBreak bool)) {
-	cm.l.Lock()
-	defer cm.l.Unlock()
-	for _, iValue := range cm.m {
-		loopBreak := cbfunc(iValue)
-		if loopBreak {
-			break
-		}
-	}
-}
-
 func (cm *CMap) ForeachItem(cbfunc func(key interface{}, value interface{}) (loopBreak bool)) {
 	cm.l.Lock()
 	defer cm.l.Unlock()
@@ -244,18 +169,6 @@ func (cm *CMap) ForeachItem(cbfunc func(key interface{}, value interface{}) (loo
 			break
 		}
 	}
-}
-
-func (cm *CMap) rLock() {
-	cm.l.RLock()
-}
-
-func (cm *CMap) rUnlock() {
-	cm.l.RUnlock()
-}
-
-func (cm *CMap) unsafeSet(key interface{}, value interface{}) {
-	cm.m[key] = value
 }
 
 func NewCMap() *CMap {
--- cmap/cmap_test.go
+++ cmap/cmap_test.go
@@ -4,34 +4,317 @@ import (
 	"testing"
 )
 
-func TestLenSetAndGet(t *testing.T) {
+func TestLen(t *testing.T) {
 	nm := NewCMap()
 	act1 := nm.Len()
-	exp1 := int(0)
+	exp1 := 0
+	nm.Set("hoge", 1000)
+	act2 := nm.Len()
+	exp2 := 1
 	if act1 != exp1 {
 		t.Errorf("error: act %v exp %v", act1, exp1)
 	}
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+}
 
+func TestSetGet(t *testing.T) {
+	nm := NewCMap()
 	nm.Set("hoge", 1000)
-	act2_1, act2_2 := nm.Get("hoge")
-	exp2_1 := 1000
-	exp2_2 := true
-	if act2_1 != exp2_1 {
-		t.Errorf("error: act %v exp %v", act2_1, exp2_1)
+	act1, act2 := nm.Get("hoge")
+	exp1 := 1000
+	exp2 := true
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
 	}
-	if act2_2 != exp2_2 {
-		t.Errorf("error: act %v exp %v", act2_2, exp2_2)
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
 	}
-
-	act3 := nm.Len()
-	exp3 := int(1)
+	act3, act4 := nm.Get("abc")
+	var exp3 interface{} = nil // value type is interface{} in cmap
+	exp4 := false
 	if act3 != exp3 {
 		t.Errorf("error: act %v exp %v", act3, exp3)
 	}
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+}
 
-	_, act4_2 := nm.Get("abc")
-	exp4_2 := false
-	if act4_2 != exp4_2 {
-		t.Errorf("error: act %v exp %v", act4_2, exp4_2)
+func TestSetIfAbsend(t *testing.T) {
+	nm := NewCMap()
+	nm.Set("hoge", 1000)
+	act0 := nm.SetIfAbsent("hoge", 100)
+	act1, act2 := nm.Get("hoge")
+	exp0 := false
+	exp1 := 1000
+	exp2 := true
+	if act0 != exp0 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	act3 := nm.SetIfAbsent("hoge2", 100)
+	act4, act5 := nm.Get("hoge2")
+	exp3 := true
+	exp4 := 100
+	exp5 := true
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+}
+
+func TestSetIfExist(t *testing.T) {
+	nm := NewCMap()
+	nm.Set("hoge", 1000)
+	act0 := nm.SetIfExist("hoge", 100)
+	act1, act2 := nm.Get("hoge")
+	exp0 := true
+	exp1 := 100
+	exp2 := true
+	if act0 != exp0 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	act3 := nm.SetIfExist("hoge2", 100)
+	act4, act5 := nm.Get("hoge2")
+	exp3 := false
+	var exp4 interface{} = nil // value type is interface{} in cmap
+	exp5 := false
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+}
+
+func TestGetWithDefault(t *testing.T) {
+	nm := NewCMap()
+	nm.Set("hoge", 1000)
+	act1 := nm.GetWithDefault("hoge", 222)
+	exp1 := 1000
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	act2 := nm.GetWithDefault("hoge2", 222)
+	exp2 := 222
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	act3, act4 := nm.Get("hoge2")
+	exp3 := 222
+	exp4 := true
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp4)
+	}
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act3, exp4)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	nm := NewCMap()
+	nm.Set("hoge", 1000)
+	nm.Set("hoge2", 100)
+	act1 := nm.Len()
+	exp1 := 2
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	act2 := nm.Delete("hoge")
+	exp2 := true
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	act3 := nm.Len()
+	exp3 := 1
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	act4 := nm.Delete("fuga")
+	exp4 := false
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	act5 := nm.Len()
+	exp5 := 1
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+}
+
+func TestClear(t *testing.T) {
+	nm := NewCMap()
+	nm.Set("hoge", 1000)
+	nm.Set("hoge2", 100)
+	act1 := nm.Len()
+	exp1 := 2
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	nm.Clear()
+	act2 := nm.Len()
+	exp2 := 0
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+}
+
+func TestOverWriteMerge(t *testing.T) {
+	nm1 := NewCMap()
+	nm1.Set("hoge", 1000)
+	nm1.Set("aaaa", 222)
+	act1 := nm1.Len()
+	exp1 := 2
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	nm2 := NewCMap()
+	nm2.Set("fuga", 2000)
+	nm2.Set("aaaa", 111)
+	act2 := nm2.Len()
+	exp2 := 2
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	nm1.OverwriteMerge(nm2)
+	act3 := nm1.Len()
+	exp3 := 3
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	act4, act5 := nm1.Get("hoge")
+	act6, act7 := nm1.Get("aaaa")
+	act8, act9 := nm1.Get("fuga")
+	exp4 := 1000
+	exp5 := true
+	exp6 := 111
+	exp7 := true
+	exp8 := 2000
+	exp9 := true
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+	if act6 != exp6 {
+		t.Errorf("error: act %v exp %v", act6, exp6)
+	}
+	if act7 != exp7 {
+		t.Errorf("error: act %v exp %v", act7, exp7)
+	}
+	if act8 != exp8 {
+		t.Errorf("error: act %v exp %v", act8, exp8)
+	}
+	if act9 != exp9 {
+		t.Errorf("error: act %v exp %v", act9, exp9)
+	}
+}
+
+func TestKeepMerge(t *testing.T) {
+	nm1 := NewCMap()
+	nm1.Set("hoge", 1000)
+	nm1.Set("aaaa", 222)
+	act1 := nm1.Len()
+	exp1 := 2
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	nm2 := NewCMap()
+	nm2.Set("fuga", 2000)
+	nm2.Set("aaaa", 111)
+	act2 := nm2.Len()
+	exp2 := 2
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	nm1.KeepMerge(nm2)
+	act3 := nm1.Len()
+	exp3 := 3
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	act4, act5 := nm1.Get("hoge")
+	act6, act7 := nm1.Get("aaaa")
+	act8, act9 := nm1.Get("fuga")
+	exp4 := 1000
+	exp5 := true
+	exp6 := 222
+	exp7 := true
+	exp8 := 2000
+	exp9 := true
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+	if act6 != exp6 {
+		t.Errorf("error: act %v exp %v", act6, exp6)
+	}
+	if act7 != exp7 {
+		t.Errorf("error: act %v exp %v", act7, exp7)
+	}
+	if act8 != exp8 {
+		t.Errorf("error: act %v exp %v", act8, exp8)
+	}
+	if act9 != exp9 {
+		t.Errorf("error: act %v exp %v", act9, exp9)
+	}
+}
+
+func TestCopy(t *testing.T) {
+	nm1 := NewCMap()
+	nm1.Set("hoge", 1000)
+	nm1.Set("fuga", 2000)
+	act1 := nm1.Len()
+	exp1 := 2
+	if act1 != exp1 {
+		t.Errorf("error: act %v exp %v", act1, exp1)
+	}
+	nm2 := nm1.Copy()
+	act2 := nm2.Len()
+	exp2 := 2
+	if act2 != exp2 {
+		t.Errorf("error: act %v exp %v", act2, exp2)
+	}
+	act3, act4 := nm2.Get("hoge")
+	act5, act6 := nm2.Get("fuga")
+	exp3 := 1000
+	exp4 := true
+	exp5 := 2000
+	exp6 := true
+	if act3 != exp3 {
+		t.Errorf("error: act %v exp %v", act3, exp3)
+	}
+	if act4 != exp4 {
+		t.Errorf("error: act %v exp %v", act4, exp4)
+	}
+	if act5 != exp5 {
+		t.Errorf("error: act %v exp %v", act5, exp5)
+	}
+	if act6 != exp6 {
+		t.Errorf("error: act %v exp %v", act6, exp6)
 	}
 }
